73cb015da763ef9463d5d40df5a802e06a104602
TommyBoyUW <sepant@uw.edu>
2022-01-30 15:12:53 -0500

    added 1 more var and operation

--- calculator.js
+++ calculator.js
@@ -1,14 +1,17 @@
 
 
+
 let a = 10;
 let x = 5;
 let y = 3;
 let z = 4;
 let value = x*y*z;
 let s = x ** 2;
+let p = value ** 2;
 
   console.log(Math.sqrt(s));
   console.log(value);
   console.log(a/(x+y))
   console.log((x-z)/(y+s))
   console.log((a+x+y)-(z+value+s)/20)
+  console.log(p)
